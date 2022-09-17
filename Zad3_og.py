0e23ec9b80a429d37681492a5f269088d490cc78
Orinies <113295774+Orinies@users.noreply.github.com>
2022-09-17 10:38:04 +0200

    Update Zad3
    Corrected code

--- Zad3_og.py
+++ Zad3_og.py
@@ -1,19 +1,33 @@
-#3.Na podstawie powyższych informacji przeskanować sieć i ustalić adres IP celu.
-from scapy.all import ARP,Ether,srp
+import mask as mask
+import netaddr
+from netaddr import ip
+from scapy.all import ARP, Ether, srp
+from scapy.layers.inet import IP, ICMP, TCP
+import netifaces as ni
 
-target_ip = netaddr.IPNetwork('%s/%s'%(ip,mask))
+# 1. Ustalić własny adres IP.
+ip = ni.ifaddresses('eth0')[ni.AF_INET][0]['addr']
+print(ip)
+
+# 2. Ustalić maskę podsieci.
+mask = ni.ifaddresses('eth0')[ni.AF_INET][0]['netmask']
+print(mask)
+
+# 3. Na podstawie powyższych informacji przeskanować sieć i ustalić adres IP celu.
+from scapy.all import ARP, Ether, srp
+
+target_ip = netaddr.IPNetwork('%s/%s' % (ip, mask))
 target_ip_str = str(target_ip)
 arp = ARP(pdst=target_ip_str)
 ether = Ether(dst="ff:ff:ff:ff:ff:ff")
 packet = ether/arp
-result = srp(packet,timeout=3,verbose=1)[0]
+result = srp(packet, timeout=3, verbose=1)[0]
 clients = []
 
-for sent,received in result:
-clients.append({'ip':received.psrc,'mac':received.hwsrc})
+for sent, received in result:
+    clients.append({'ip': received.psrc, 'mac': received.hwsrc})
 
 print("Available devices in the network:")
-print("IP" + " " * 18 + "MAC")
-
+print("IP" + " "*18+"MAC")
 for client in clients:
-print("{:16}{}".format(client['ip'],client['mac']))
+    print("{:16}    {}".format(client['ip'], client['mac']))
